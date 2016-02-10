34573679fc597c30b457f1dbd040ac1c97a0ae73
Matthias Müller <matthias_mueller@tu-dresden.de>
2016-02-10 11:03:11 +0100

    Create pbf2buldings.sh

---
+++ buildings/pbf2buldings.sh
@@ -0,0 +1,5 @@
+#osmconvert sachsen-latest.osm.pbf -b=13.5,50.9,14.0,51.2 --verbose -o=dd_clip.pbf
+osmconvert sachsen-latest.osm.pbf -B=clip.poly --verbose -o=dd_clip.pbf
+osmconvert dd_clip.pbf >dd_clip.osm
+osmfilter dd_clip.osm --keep="building=*" --drop="building=no" -o=dd_clip_b.osm
+ogr2ogr --config OSM_CONFIG_FILE osmconf_buildings.ini -progress dd_clip_b.shp dd_clip_b.osm -skipfailure
